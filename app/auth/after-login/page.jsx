ec8a3efd0e3acb56f53ae54fe74430b450aa0419
0kana0 <cyvery2543@gmail.com>
2026-01-29 15:52:56 +0700

    ลองกลับไปดู

--- app/auth/after-login/page.jsx
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic";
-
-const AfterLoginClient = dynamic(() => import("./AfterLoginClient"), { ssr: false });
-
-export default function Page() {
-  return <AfterLoginClient />;
-}
